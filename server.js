1aa1b85fd44c6368f2a751dc972f93ca64292333
mahdi ghazaei <mahdighazaei8@gmail.com>
2022-10-13 09:13:42 +0000

    Update 4 files

--- server.js
+++ server.js
@@ -1,25 +1,11 @@
 "use strict";
-require("dotenv").config();
 const express = require("express");
-const passport = require("passport");
-const session = require("express-session");
+const routes = require("./routes.js");
+const auth = require("./auth.js");
 const myDB = require("./connection");
-const ObjectID = require("mongodb").ObjectID;
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
-const LocalStrategy = require("passport-local");
+require("dotenv").config();
 const app = express();
-
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
-    cookie: { secure: false }
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
@@ -29,118 +15,13 @@ app.set("view engine", "pug");
 
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
-
-  // Be sure to change the title
-  app.route("/").get((req, res) => {
-    //Change the response to render the Pug template
-    res.render("pug", {
-      title: "Connected to Database",
-      message: "Please login",
-      showLogin: true
-    });
-  });
-
-  app.post(
-    "/login",
-    passport.authenticate("local", { failureRedirect: "/" }),
-    (req, res) => {
-      res.redirect("/profile");
-    }
-  );
-
-  function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect("/");
-  }
-
-  app.get("/profile", ensureAuthenticated, (req, res) => {
-    res.render("pug/profile", {
-      username: req.user.username
-    });
-  });
-
-  app.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/");
-  });
-
-  app.route("/register").post(
-    (req, res, next) => {
-      myDataBase.findOne({ username: req.body.username }, function (err, user) {
-        if (err) {
-          next(err);
-        } else if (user) {
-          res.redirect("/");
-        } else {
-          myDataBase.insertOne(
-            {
-              username: req.body.username,
-              password: req.body.password
-            },
-            (err, doc) => {
-              if (err) {
-                res.redirect("/");
-              } else {
-                // The inserted document is held within
-                // the ops property of the doc
-                next(null, doc.ops[0]);
-              }
-            }
-          );
-        }
-      });
-    },
-    passport.authenticate("local", { failureRedirect: "/" }),
-    (req, res, next) => {
-      res.redirect("/profile");
-    }
-  );
-
-  app.use((req, res, next) => {
-    res.status(404).type("text").send("Not Found");
-  });
-
-  // Serialization and deserialization here...
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  });
-
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
-
-  passport.use(
-    new LocalStrategy(function (username, password, done) {
-      myDataBase.findOne({ username: username }, function (err, user) {
-        console.log("User " + username + " attempted to log in.");
-        if (err) {
-          return done(err);
-        }
-        if (!user) {
-          return done(null, false);
-        }
-        if (password !== user.password) {
-          return done(null, false);
-        }
-        return done(null, user);
-      });
-    })
-  );
-
-  // Be sure to add this...
+  auth(app, myDataBase);
+  routes(app, myDataBase);
 }).catch((e) => {
   app.route("/").get((req, res) => {
     res.render("pug", { title: e, message: "Unable to login" });
   });
 });
-
-// app.route("/").get((req, res) => {
-//   res.render("pug/index", { title: "Hello", message: "Please login" });
-// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
